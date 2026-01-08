c7729ffb1d2e74f3c3d8dcdd587177b51dba0a7f
Biplav Poudel <biplavpoudel764@gmail.com>
2026-01-08 10:22:23 +0545

    updated commit ptr for buildroot

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,24 +6,28 @@
 ##############################################################
 
 #Referencing my assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '8fae63497f4552d62aad4b7555e0b2bf49717815'
+AESD_ASSIGNMENTS_VERSION = 4b7d44f367728a9b39cafa637c05fc081b4d5dff
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly with ssh keys and the automated build/test system.
 # Our site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-biplavpoudel.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-biplavpoudel.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) CC="$(TARGET_CC)" -C $(@D)/finder-app all
 endef
 
 # Adding our writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
+	@echo "Installing configuration files from ${AESD_ASSIGNMENTS_SITE}..."
 	$(INSTALL) -d 0755 $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0644 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
-
-	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
-
+	
+	@echo "Installing autotest scripts from $(AESD_ASSIGNMENTS_SITE)..."
+	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/usr/bin
+	
+	@echo "Installing binaries for finder-app from $(AESD_ASSIGNMENTS_SITE)..."
+	$(INSTALL) -d 0755 $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin
 	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin
